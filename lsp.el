98cddf2225c097ffde9aea36f361ba283ca06f73
Andrei Kononov <flowneee3@gmail.com>
2025-01-05 02:15:38 +0400

    Integrate emacs-lsp-booster for LSP

--- lsp.el
+++ lsp.el
@@ -1,18 +1,53 @@
 ;; NOTE: flycheck is enabled in init.el
 
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+              (setcar orig-result command-from-exec-path))
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :hook (lsp-mode . flycheck-mode)
   :init
   (setq lsp-keymap-prefix "C-c C-l")
   :bind (:map lsp-mode-map
-              ([remap xref-find-apropos] . consult-lsp-symbols))
+              ([remap xref-find-apropos] . consult-lsp-symbols)
+              ([remap xref-find-definitions] . lsp-find-definition)
+              ([remap xref-find-references] . lsp-find-references))
   :custom
   ((lsp-enable-snippet t)
    (lsp-log-io nil)
    (lsp-prefer-flymake nil)
    (lsp-lens-enable nil)
-   (lsp-enable-symbol-highlighting t)))
+   (lsp-enable-symbol-highlighting t))
+  :config
+  ;; Integrate emacs-lsp-booster https://github.com/blahgeek/emacs-lsp-booster?tab=readme-ov-file#configure-lsp-mode
+  (advice-add (if (progn (require 'json)
+                         (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
